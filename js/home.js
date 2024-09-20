3f93c1f84d291bcf46a325286466d05b36ec82ea
amerry539 <amerry539@gmail.com>
2024-09-20 16:54:48 +0600

    third

--- js/home.js
+++ js/home.js
@@ -4,7 +4,10 @@
 /* s-1: add event handler
        prevent page reload afted from submit
    s-2: get money to be added to the acccount balance
-   s-3: 
+   s-3: verify pin number 
+   step : 4 get the current balance
+   step-5: add addmore
+   step-6: update the balance in the UI / DOM
 */
 
 // step-1: add event handler to the add money button inside the formet
@@ -42,12 +45,50 @@ document.getElementById('btn_add_money')
 
             // step-6: update the balance in the UI / DOM
             document.getElementById('account_balance').innerText = newBalance;
+        }
+        else{
+            alert('Failed to add money! Please try again.')
+        }
 
 
+});
+
+// cash out 
+
+document.getElementById('btn_add_money')
+    .addEventListener('click', function(event){
+        // prevent page reload after from submit
+        event.preventDefault();
+        console.log('money add btn clicked');
+        // step -2 : get money to be added to the account
+        const addMoneyInput = document.getElementById('input_add_money').value;
+        console.log(addMoneyInput);
+
+        // get the pin number provided
+        const pinNumberInput = document.getElementById('input_pin_number').value;
+        console.log(pinNumberInput);
+
+        // step -3: verify pin number 
+        // wrong way to validate pin number
+        if(pinNumberInput === '1234'){
+            console.log('adding money to your account')
+
+            // step : 4 get the current balance
+
+            // text hole --> innerText
+            // input filed ---> value
+            const balance = document.getElementById('account_balance').innerText;
+            console.log(balance);
+
+            // step-5: add addmore
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
 
 
-
-
+            // step-6: update the balance in the UI / DOM
+            document.getElementById('account_balance').innerText = newBalance;
         }
         else{
             alert('Failed to add money! Please try again.')
