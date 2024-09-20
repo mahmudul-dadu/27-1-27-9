bf3ca0b7baf3bc98a8c25f79ca2b0c556bccfc51
amerry539 <amerry539@gmail.com>
2024-09-20 16:31:33 +0600

    second

--- js/home.js
+++ js/home.js
@@ -11,14 +11,47 @@
 document.getElementById('btn_add_money')
     .addEventListener('click', function(event){
         // prevent page reload after from submit
-        //event.preventDefault();
+        event.preventDefault();
         console.log('money add btn clicked');
         // step -2 : get money to be added to the account
         const addMoneyInput = document.getElementById('input_add_money').value;
         console.log(addMoneyInput);
 
         // get the pin number provided
-        const pinNumberProvided = document.getElementById('input_pin_number').value;
-        console.log(pinNumberProvided);
+        const pinNumberInput = document.getElementById('input_pin_number').value;
+        console.log(pinNumberInput);
+
+        // step -3: verify pin number 
+        // wrong way to validate pin number
+        if(pinNumberInput === '1234'){
+            console.log('adding money to your account')
+
+            // step : 4 get the current balance
+
+            // text hole --> innerText
+            // input filed ---> value
+            const balance = document.getElementById('account_balance').innerText;
+            console.log(balance);
+
+            // step-5: add addmore
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+
+            // step-6: update the balance in the UI / DOM
+            document.getElementById('account_balance').innerText = newBalance;
+
+
+
+
+
+
+        }
+        else{
+            alert('Failed to add money! Please try again.')
+        }
+
 
 });
